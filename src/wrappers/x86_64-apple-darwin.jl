da70537084b481c387cf89ab134356830b42fe84
jlbuild <juliabuildbot@gmail.com>
2021-03-10 00:36:04 +0000

    LCIO_jll build 2.16.1+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -3,19 +3,19 @@ export liblcio, libsio
 
 using Zlib_jll
 JLLWrappers.@generate_wrapper_header("LCIO")
-JLLWrappers.@declare_library_product(liblcio, "@rpath/liblcio.2.15.dylib")
+JLLWrappers.@declare_library_product(liblcio, "@rpath/liblcio.2.16.dylib")
 JLLWrappers.@declare_library_product(libsio, "@rpath/libsio.0.0.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(Zlib_jll)
     JLLWrappers.@init_library_product(
         liblcio,
-        "lib/liblcio.2.15.4.dylib",
+        "lib/liblcio.2.16.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libsio,
-        "lib/libsio.0.0.2.dylib",
+        "lib/libsio.0.0.4.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
